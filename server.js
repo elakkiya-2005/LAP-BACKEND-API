4cee880a56af35b515e470e21cda1f87e51b3652
elakkiya-2005 <elakkiya10112005@gmail.com>
2025-08-22 15:20:22 +0530

    Added CRUD API

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://elakkiya10112005:<ELAKKIYA>@cluster0.uiaskha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://elakkiya10112005:ELAKKIYA@cluster0.uiaskha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
