74bc23d21f36cb988cc6697b1f28701822b700a2
elakkiya-2005 <elakkiya10112005@gmail.com>
2025-08-22 14:59:37 +0530

    Inital PRoject

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express =require('express');
-const mongoose =require('monogoose');
-const bodyparser=require('body-parser');
-const cors=require('cors');
+const express= require('express')
+const mongoose= require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app=express();
 app.use(cors());
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://elakkiya10112005:ELAKKIYA@cluster0.uiaskha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
-    useNewUrlparser:true,
-    useUnifiedTopology:true
+    useNewUrlParser: true,
+    useUnifiedTopology: true    
+
 })
-.then(() => console.log("MongoDB Atlas Connected"))
+.then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
+
+const itemRoutes=require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+app.listen(5000,()=>console.log("Server is running on port 5000"));
